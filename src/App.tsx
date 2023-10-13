cb281db2432798df716a608b85a6c9954eaf9a80
samuelpoutsalo <samuel.poutsalo@gmail.com>
2023-10-13 09:11:21 +0300

    Changed text to be more clear

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,10 @@ function App() {
     }
   };
 
+  const notReady = () => {
+    window.alert("API website will be by next week");
+  };
+
   return (
     <div className="App">
       <header className="nav">
@@ -240,14 +244,14 @@ function App() {
             <div className="projects-content">
               <div className="site-preview">
                 <div className="image-preview normal">
-                  <a href="">
+                  <a onClick={notReady}>
                     <img src={under} />
                   </a>
                 </div>
                 <div className="text-preview">
                   <h3 className="black">
-                    Countries API website{" "}
-                    <span className="date">(TBD 2023)</span>
+                    (Not yet complete) API website
+                    <span className="date"> (TBD)</span>
                   </h3>
                   <p className="light">
                     A simple but responsive website where the user is be able
@@ -261,8 +265,8 @@ function App() {
                     <p>SCSS</p>
                     <p>TypeScript</p>
                   </div>
-                  <div className="code-links">
-                    <a href="">
+                  <div className="code-links" onClick={notReady}>
+                    <a>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
@@ -272,7 +276,7 @@ function App() {
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                       </svg>
                     </a>
-                    <a href="">
+                    <a>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
